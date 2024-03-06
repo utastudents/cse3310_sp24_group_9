456b26b5adfe66e2c4cd19a96db7227838cf523f
Vincent Tran <tranvincent73@gmail.com>
2024-03-05 20:08:52 -0600

    Update

---
+++ src/Color.java
@@ -0,0 +1,7 @@
+package src;
+public enum Color {
+    purple,
+    green, 
+    blue, 
+    orange
+}
---
+++ src/Game.java
@@ -0,0 +1,6 @@
+package src;
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("The game is starting!");
+    }
+}
