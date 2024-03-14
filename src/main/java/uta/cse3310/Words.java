847b9d1c1d411b27d65aba62622fec954eefcaa3
Bigyan Dahal <bxd3918@mavs.uta.edu>
2024-03-13 22:34:50 -0500

    added filename parameter

--- src/main/java/uta/cse3310/Words.java
+++ src/main/java/uta/cse3310/Words.java
@@ -2,12 +2,13 @@ package uta.cse3310;
 
 public class Words {
     private String words;
+    private String filename;
 
     void filter(){
         // TODO: implement
     }    
 
-    void GetWords(String words){
+    void GetWords(String words, String filename){
         // TODO: implement
     }
 }
