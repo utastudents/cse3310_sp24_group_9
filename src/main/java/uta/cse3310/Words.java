0312cfcb98aa29ee54eade97a977d78ccfe16beb
Isaac Elizarraraz <ixe6536@mavs.uta.edu>
2024-03-21 18:15:23 -0500

    added method&field

--- src/main/java/uta/cse3310/Words.java
+++ src/main/java/uta/cse3310/Words.java
@@ -1,14 +1,18 @@
 package uta.cse3310;
 
 public class Words {
-    private String words;
+    private String words[];
+    private String foundWords[];
     private String filename;
 
-    void filter(){
+    void filterWords(){
         // TODO: implement
     }    
 
     void GetWords(String words, String filename){
         // TODO: implement
+    }
+    void updateFoundWords(String foundWords[]){
+        //TODO: implement
     }
 }
