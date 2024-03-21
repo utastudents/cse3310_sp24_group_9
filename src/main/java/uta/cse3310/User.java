9d9a514e9bda065e9765c19e10568639c2d3e490
Vincent Tran <tranvincent73@gmail.com>
2024-03-21 18:30:45 -0500

    Update

--- src/main/java/uta/cse3310/User.java
+++ src/main/java/uta/cse3310/User.java
@@ -4,10 +4,11 @@ public class User {
     private int ID;
     private int score;
     public String name;
-    public colors color;
+    public Colors color;
     public boolean ready;
     private String words;
     private boolean crown;
+    private String[][] foundWords;
 
     public String toString(){
         // TODO: implement
