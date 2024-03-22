6d9eefeaf8aacb92f61a20a1638c42ae166cc7ef
lhd2156 <louis@LAPTOP-DRCR3NN8.localdomain>
2024-03-21 22:30:55 -0500

    Finished User Methods

--- src/main/java/uta/cse3310/User.java
+++ src/main/java/uta/cse3310/User.java
@@ -4,11 +4,35 @@ public class User {
     private int ID;
     private int score;
     public String name;
-    public Colors color;
+    public colors color;
     public boolean ready;
     private String words;
     private boolean crown;
     private String[][] foundWords;
+
+    private void userName(String name){
+        // TODO implement
+    }
+
+    private void readyUp(boolean ready){
+        // TODO implement
+    }
+
+    private void updateUserWords(String foundWord){
+        // TODO implement
+    }
+
+    private void updateScore(){
+        // TODO implement
+    }
+
+    private void userColor(colors color){
+        // TODO implement
+    }
+
+    private void userCrown(boolean crown){
+        // TODO implement
+    }
 
     public String toString(){
         // TODO: implement
