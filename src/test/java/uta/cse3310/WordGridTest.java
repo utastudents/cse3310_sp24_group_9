25c6ac826b0e51b0dd3e37e297e5ac3ce1428d16
Isaac Elizarraraz <ixe6536@mavs.uta.edu>
2024-04-26 00:36:18 -0500

    Isaac and Bigyan density

--- src/test/java/uta/cse3310/WordGridTest.java
+++ src/test/java/uta/cse3310/WordGridTest.java
@@ -15,15 +15,16 @@ public class WordGridTest {
   public void testWordFill() {
     WordGrid wordGrid = new WordGrid();
     wordGrid.WordFill();
-    float maxCharacters = wordGrid.getVariationDensity("total");
-    float requiredVariationDensity = wordGrid.getVariationDensity("variation")/maxCharacters;
-    float requiredUpDensity = wordGrid.getVariationDensity("verticalUp");
-    float requiredDownDensity = wordGrid.getVariationDensity("verticalDown");
-    float requiredHorizontalDensity = wordGrid.getVariationDensity("horizontal");
-    float requiredDiagonalUpDensity = wordGrid.getVariationDensity("diagonalUp");
-    float requiredDiagonalDownDensity = wordGrid.getVariationDensity("diagonalDown");
+    float totalWords = wordGrid.getVariationDensity(5);
+    float requiredVariationDensity = wordGrid.getVariationDensity(6);
+    float requiredUpDensity = wordGrid.getVariationDensity(4);
+    float requiredDownDensity = wordGrid.getVariationDensity(1);
+    float requiredHorizontalDensity = wordGrid.getVariationDensity(0);
+    float requiredDiagonalUpDensity = wordGrid.getVariationDensity(3);
+    float requiredDiagonalDownDensity = wordGrid.getVariationDensity(2);
     /*
-     * assertTrue(requiredUpDensity >= requiredVariationDensity);
+     * 
+    assertTrue(requiredUpDensity >= requiredVariationDensity);
     assertTrue(requiredDownDensity >= requiredVariationDensity);
     assertTrue(requiredDiagonalUpDensity >= requiredVariationDensity);
     assertTrue(requiredDiagonalDownDensity >= requiredVariationDensity);
@@ -32,12 +33,25 @@ public class WordGridTest {
     */
     
 
+    
+
+    System.out.println("\nRequired number of words per variation " + totalWords*0.15f);
     System.out.println("Required variation density: " + requiredVariationDensity);
-    System.out.println("\nHorizontal density: " + requiredHorizontalDensity);
-    System.out.println("\nVertical Up density: " + requiredUpDensity);
-    System.out.println("\nVertical Down density: " + requiredDownDensity);
-    System.out.println("\nDiagonal Up density: " + requiredDiagonalUpDensity);
-    System.out.println("\nDiagonal Down density: " + requiredDiagonalDownDensity);
+
+    System.out.println("\nHorizontal words: " + requiredHorizontalDensity * totalWords);
+    System.out.println("Horizontal density: " + requiredHorizontalDensity);
+
+    System.out.println("\nVertical Up words: " + requiredUpDensity * totalWords);
+    System.out.println("Vertical Up density: " + requiredUpDensity);
+
+    System.out.println("\nVertical Down words: " + requiredDownDensity * totalWords);
+    System.out.println("Vertical Down density: " + requiredDownDensity);
+
+    System.out.println("\nDiagonal Up words: " + requiredDiagonalUpDensity * totalWords);
+    System.out.println("Diagonal Up density: " + requiredDiagonalUpDensity);
+
+    System.out.println("\nDiagonal Down words: " + requiredDiagonalDownDensity * totalWords);
+    System.out.println("Diagonal Down density: " + requiredDiagonalDownDensity);
 
 
 
@@ -65,11 +79,11 @@ public class WordGridTest {
   }
 
   
-  public void testFillVertical() {
+  public void testFillVerticalDown() {
     WordGrid wordGrid = new WordGrid();
-    assertTrue(wordGrid.fillVertical("test")); // Test a word that can be placed vertically
+    assertTrue(wordGrid.fillVerticalDown("test")); // Test a word that can be placed vertically
     assertFalse(
-      wordGrid.fillVertical(
+      wordGrid.fillVerticalDown(
         "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
       )
     ); // Test a word that cannot be placed vertically due to length
